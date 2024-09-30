32bae46675ed30e7f3e7065d0d9d1fb3608eee52
AlinaGryshchenko <alinahryshchenkoios@gmail.com>
2024-09-30 12:59:19 +0100

    first commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Алина Лошакова on 30.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
